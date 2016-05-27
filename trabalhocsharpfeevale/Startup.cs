056a936e8af0bc7e280e0a8f3efe5c187aaca9bc
Pablo Brodt <pablobrodt@gmail.com>
2016-05-27 11:09:04 -0300

    Projeto inicial

---
+++ trabalhocsharpfeevale/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(trabalhocsharpfeevale.Startup))]
+namespace trabalhocsharpfeevale
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
