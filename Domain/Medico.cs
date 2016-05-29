8b68ed887dce9d9a10c5df453f537441aebc2eb8
Pablo Brodt <pablobrodt@gmail.com>
2016-05-29 17:51:21 -0300

    Adicionar projeto domain base

---
+++ Domain/Medico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Medico
+    {
+        public int IDMedico { get; set; }
+
+        public int CRM { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Email { get; set; }
+
+        public bool AtendePorConvenio { get; set; }
+
+        public bool TemClinica { get; set; }
+
+        public string WebsiteBlog { get; set; }
+
+    }
+}
---
+++ Domain/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Usuario
+    {
+        public int IDUsuario { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Login { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
