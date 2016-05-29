20926f2132441921dd7ca783d8eec16076bf438e
Pablo Brodt <pablobrodt@gmail.com>
2016-05-29 17:56:20 -0300

    Adicionar annotations de nomes de tabelas

--- Domain/Cidade.cs
+++ Domain/Cidade.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain
-{
+{   
+    [Table("Cidades")]
     public class Cidade
     {
-
         public int IDCidade { get; set; }
 
         public string Nome { get; set; }
-
     }
 }
