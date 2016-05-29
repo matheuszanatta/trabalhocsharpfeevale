a968b948cbe525fc27042703086a002b14087edf
Pablo Brodt <pablobrodt@gmail.com>
2016-05-29 18:58:52 -0300

    Alteração na estrutura

---
+++ trabalhocsharpfeevale/Models/Especialidade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{   
+    [Table("Especialidades")]
+    public class Especialidade
+    {
+        public int IDEspecialidade { get; set; }
+
+        public string Nome { get; set; }
+
+    }
+}
---
+++ trabalhocsharpfeevale/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{   
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        public int IDUsuario { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Login { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
